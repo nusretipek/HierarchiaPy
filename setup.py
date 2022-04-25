ea02dfd9d637cab71a2cceea294cdf5711281e0c
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-04-25 15:21:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ INSTALL_REQUIRES = [
     "networkx>=2.0"
 ]
 
-PACKAGES=['HierarchiaPy']
+PACKAGES=["HierarchiaPy", "methods"]
 CLASSIFIERS = [
               "Development Status :: 2 - Pre-Alpha",
               "Programming Language :: Python :: 3",
