3fa955abe4cb75375cde7b68a8e93040f96db4f9
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-04-26 14:40:07 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,11 @@ version = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.todo',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
 ]
 
