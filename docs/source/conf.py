857991792c4934d0a78be4c46c13350138a143e4
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-04-26 13:54:17 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HierarchiaPy'
+copyright = '2022, Nusret Ipek'
+author = 'nusretipek'
 
 release = '0.1'
 version = '0.1.0'
