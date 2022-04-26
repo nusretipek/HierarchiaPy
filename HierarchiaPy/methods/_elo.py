437219bb0ad2d3a9fd0bf1b500b9cf42b0e49806
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-04-26 11:24:17 +0200

    Update _elo.py

--- HierarchiaPy/methods/_elo.py
+++ HierarchiaPy/methods/_elo.py
@@ -3,6 +3,61 @@ from scipy.special import erfc
 
 def elo(self, start_value=1000, K=100, normal_probability=False):
 
+    """Elo rating from an interaction dataframe.
+
+    Parameters
+    ----------
+    :param self: reference to the current instance of the class
+    :param start_value: float
+        Parameter of the Elo function that determines the initial scores. It does not have
+        an effect on relative differences after the calculation. (1000)
+    :param normal_probability: Boolean
+        Adjust the calculation of expected win/loss probabilities; default is Logistic (False),
+        the normal probabilities are calculated using standard normal tables (True). For normalised probabilities,
+        refer to https://handbook.fide.com
+    :param K: float
+        Parameter of the Elo function that acts as a factor and determines the speed at which scores change
+        after an interaction. Optimization might be a good idea but not implemented yet. (100)
+
+    Returns
+    -------
+    elo_dict : dict
+        Elo ratings, keys are individual names derived from the Dataframe and values are Elo scores
+        (rounded to 4 decimal places)
+
+    See also
+    --------
+    https://mathworld.wolfram.com/Erfc.html
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.erfc.html
+
+    Notes
+    -----
+    From Wikipedia:
+    The Elo rating system is a method for calculating the relative skill levels of players
+    in zero-sum games such as chess. It is named after its creator Arpad Elo, a Hungarian-American physics professor.
+    The Elo system was originally invented as an improved chess-rating system over the previously used Harkness system,
+    but is also used as a rating system in association football, American football, baseball, basketball, pool,
+    table tennis, Go, board games such as Scrabble and Diplomacy, and esports.
+
+    The difference in the ratings between two players serves as a predictor of the outcome of a match.
+    Two players with equal ratings who play against each other are expected to score an equal number of wins.
+    A player whose rating is 100 points greater than their opponent's is expected to score 64%;
+    if the difference is 200 points, then the expected score for the stronger player is 76%.
+
+    References
+    ----------
+    * https://en.wikipedia.org/wiki/Elo_rating_system
+    * Elo, A. E. 1978. The Rating of Chess Players, Past and Present. New York: Arco.
+    * Albers, P. C. H. & de Vries, H. 2001.
+      Elo-rating as a tool in the sequential estimation of dominance strengths.
+      Animal Behaviour, 61, 489-495. (DOI: 10.1006/anbe.2000.1571)
+
+    Examples
+    --------
+    1. TBC
+    
+    """
+
     # check whether Pandas dataframe provided
     if not hasattr(self, 'df'):
         raise ValueError('Elo rating depends on sequence of wins/loses, computation uses Pandas dataframe, '
