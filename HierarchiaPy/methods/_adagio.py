08411dabeb7cc555b48835de74e6afec85144cea
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-04-27 16:11:07 +0200

    Update _adagio.py

--- HierarchiaPy/methods/_adagio.py
+++ HierarchiaPy/methods/_adagio.py
@@ -4,6 +4,53 @@ import numpy as np
 
 
 def adagio(self, preprocessing=False, plot_network=False, rank='topological'):
+
+    """ADAGIO from an interaction dataframe/matrix.
+
+    Parameters
+    ----------
+    :param self: reference to the current instance of the class
+
+    :param preprocessing: boolean
+        preprocessing of the initial matrix according to the original paper [Douglas, 2016].  For a given dominance
+        network, the preprocessed weight of the edge between the individual A that appears dominant and the individual B
+        that appears subordinate is set to the difference in the number of interactions won by A and the number
+        of interactions won by B. (False)
+
+    :param plot_network: boolean
+        Simple network plot of the derived directed graph network from the dataframe/matrix. (False)
+
+    :param rank: str
+        Final ranking algorithm after the ADAGIO iterations. Three options are possible; 'topological' that yields
+        topological order of the final matrix. 'top' and 'bottom' are self-explanatory and can be found at original
+        paper. (topological)
+
+    Returns
+    -------
+    rank_dict : list
+        Rankings from the ADAGIO algorithm, ranked based on the ranking parameter chosen.
+
+    See also
+    --------
+    https://networkx.org/documentation/networkx-1.9/reference/generated/networkx.algorithms.components.strongly_connected.strongly_connected_components.html
+    https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.dag.topological_sort.html
+
+    Notes
+    -----
+    ADAGIO, for assessing the structure of dominance networks. ADAGIO computes dominance hierarchies, in the form of
+    directed acyclic graphs, to represent the dominance relations of a given group of animals. Thus far, most
+    methods for computing dominance ranks assume implicitly that the dominance relation is a total order
+    of the individuals in a group. ADAGIO does not assume or require this to be always true, and is hence
+    more appropriate for analysing dominance hierarchies that are not strongly linear. (Douglas, 2017)
+
+    References
+    ----------
+    * Douglas, P. H., Ngomo, A. C. N., & Hohmann, G. (2017). A novel approach for dominance assessment in gregarious
+      species: ADAGIO. Animal Behaviour, 123, 21-32.    * Elo, A. E. 1978. The Rating of Chess Players,
+      Past and Present. New York: Arco.
+
+    """
+
     mat = self.mat.astype('int64')
 
     if preprocessing:
