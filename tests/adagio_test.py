c180a85990a0f238db7700936fce28df44e41790
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-04-28 14:19:28 +0200

    Update adagio_test.py

--- tests/adagio_test.py
+++ tests/adagio_test.py
@@ -19,28 +19,28 @@ mat = np.array([[0, 1, 2, 0, 0, 0],
 # ADAGIO Elo test
 
 def test_adagio_topological():
-    hier_mat = HierarchiaPy(mat, np.array(['a', 'b', 'c', 'd', 'e', 'f']))
+    hier_mat = Hierarchia(mat, np.array(['a', 'b', 'c', 'd', 'e', 'f']))
     adagio = hier_mat.adagio(preprocessing=True, plot_network=False, rank='topological')
     assert (isinstance(adagio, dict))
     assert (len(adagio) == len(hier_mat.indices))
     assert(adagio == {'a': 0, 'f': 1, 'c': 2, 'd': 3, 'e': 4, 'b': 5})
     
 def test_adagio_top():
-    hier_mat = HierarchiaPy(mat, np.array(['a', 'b', 'c', 'd', 'e', 'f']))
+    hier_mat = Hierarchia(mat, np.array(['a', 'b', 'c', 'd', 'e', 'f']))
     adagio = hier_mat.adagio(preprocessing=True, plot_network=False, rank='top')
     assert (isinstance(adagio, dict))
     assert (len(adagio) == len(hier_mat.indices))
     assert(adagio == {'a': 0, 'f': 0, 'c': 1, 'd': 2, 'e': 2, 'b': 3})
     
 def test_adagio_bottom():
-    hier_mat = HierarchiaPy(mat, np.array(['a', 'b', 'c', 'd', 'e', 'f']))
+    hier_mat = Hierarchia(mat, np.array(['a', 'b', 'c', 'd', 'e', 'f']))
     adagio = hier_mat.adagio(preprocessing=True, plot_network=False, rank='bottom')
     assert (isinstance(adagio, dict))
     assert (len(adagio) == len(hier_mat.indices))
     assert(adagio == {'b': 3, 'e': 3, 'd': 2, 'c': 1, 'f': 1, 'a': 0})
 
 def test_adagio_no_pre_bottom():
-    hier_mat = HierarchiaPy(mat, np.array(['a', 'b', 'c', 'd', 'e', 'f']))
+    hier_mat = Hierarchia(mat, np.array(['a', 'b', 'c', 'd', 'e', 'f']))
     adagio = hier_mat.adagio(preprocessing=False, plot_network=False, rank='bottom')
     assert (isinstance(adagio, dict))
     assert (len(adagio) == len(hier_mat.indices))
