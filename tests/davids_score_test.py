14907b5f5fe665f9402f501f23346da50985afeb
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-06-23 16:07:59 +0200

    Update davids_score_test.py

--- tests/davids_score_test.py
+++ tests/davids_score_test.py
@@ -24,3 +24,31 @@ def test_davids_scores():
     assert (isinstance(davids_scores, dict))
     assert (len(davids_scores) == len(hier_mat.indices))
     assert(davids_scores == {'a': 8.4444, 'b': 1.6111, 'c': -2.3333, 'd': -3.6667, 'e': -4.0556})
+    
+def test_davids_scores_dij_no_normal():
+    hier_mat = Hierarchia(mat_hemelrijk_table_2_1, np.array(['a', 'b', 'c', 'd', 'e']))
+    davids_scores = hier_mat.davids_score(method='Dij', normalize=False)
+    assert (isinstance(davids_scores, dict))
+    assert (len(davids_scores) == len(hier_mat.indices))
+    assert(davids_scores == {'a': 7.381, 'b': 1.381, 'c': -2.0714, 'd': -3.2143, 'e': -3.4762})
+
+def test_davids_scores_dij_normal():
+    hier_mat = Hierarchia(mat_hemelrijk_table_2_1, np.array(['a', 'b', 'c', 'd', 'e']))
+    davids_scores = hier_mat.davids_score(method='Dij', normalize=True)
+    assert (isinstance(davids_scores, dict))
+    assert (len(davids_scores) == len(hier_mat.indices))
+    assert(davids_scores == {'a': 3.4762, 'b': 2.2762, 'c': 1.5857, 'd': 1.3571, 'e': 1.3048})
+
+def test_davids_scores_pij_no_normal():
+    hier_mat = Hierarchia(mat_hemelrijk_table_2_1, np.array(['a', 'b', 'c', 'd', 'e']))
+    davids_scores = hier_mat.davids_score(method='Pij', normalize=False)
+    assert (isinstance(davids_scores, dict))
+    assert (len(davids_scores) == len(hier_mat.indices))
+    assert(davids_scores == {'a': 8.4444, 'b': 1.6111, 'c': -2.3333, 'd': -3.6667, 'e': -4.0556})
+
+def test_davids_scores_pij_normal():
+    hier_mat = Hierarchia(mat_hemelrijk_table_2_1, np.array(['a', 'b', 'c', 'd', 'e']))
+    davids_scores = hier_mat.davids_score(method='Pij', normalize=True)
+    assert (isinstance(davids_scores, dict))
+    assert (len(davids_scores) == len(hier_mat.indices))
+    assert(davids_scores == {'a': 3.6889, 'b': 2.3222, 'c': 1.5333, 'd': 1.2667, 'e': 1.1889})
