1ddcc7a2dc0d91706a7f63de717ea075dd146bf0
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-06-23 15:19:03 +0200

    Update _network_graph.py

--- HierarchiaPy/methods/_network_graph.py
+++ HierarchiaPy/methods/_network_graph.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+import warnings
 
 
 def directed_network_graph(self, **kwargs):
@@ -15,20 +16,29 @@ def directed_network_graph(self, **kwargs):
     network_graph_int = nx.relabel_nodes(network_graph_int, {idx: name for idx, name in enumerate(self.indices)})
 
     if 'fig_size' in kwargs:
-        plt.figure(figsize=kwargs['fig_size'])
+        figure = plt.figure(figsize=kwargs['fig_size'])
         del kwargs['fig_size']
+    else:
+        figure = plt.figure()
 
-    pos = nx.planar_layout(network_graph)
-    nx.draw_networkx(network_graph,
-                     pos,
-                     **kwargs,
-                     node_size=1000,
-                     linewidths=2,
-                     node_shape='8')
+    try:
+        pos = nx.planar_layout(network_graph)
+        nx.draw_networkx(network_graph,
+                         pos,
+                         **kwargs,
+                         node_size=1000,
+                         linewidths=2,
+                         node_shape='8')
 
-    weights = nx.get_edge_attributes(network_graph, 'weight')
-    weights_int = nx.get_edge_attributes(network_graph_int, 'weight')
-    weights_final = {key: (weights_int[key], weights[key]) for key in weights}
-    nx.draw_networkx_edge_labels(network_graph, pos, edge_labels=weights_final)
-    plt.tight_layout()
-    return plt
+        weights = nx.get_edge_attributes(network_graph, 'weight')
+        weights_int = nx.get_edge_attributes(network_graph_int, 'weight')
+        weights_final = {key: (weights_int[key], weights[key]) for key in weights}
+        nx.draw_networkx_edge_labels(network_graph, pos, edge_labels=weights_final)
+        plt.tight_layout()
+        figure.canvas.draw()
+        plt.show()
+        return figure
+
+    finally:
+        warnings.warn('Layout is not suitable for planar!')
+        return None
