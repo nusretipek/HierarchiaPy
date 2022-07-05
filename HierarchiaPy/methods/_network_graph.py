dfb5bacee4efd5e24c3e43d0375cae61f7a3c6d3
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-07-05 15:05:42 +0200

    Update _network_graph.py

--- HierarchiaPy/methods/_network_graph.py
+++ HierarchiaPy/methods/_network_graph.py
@@ -1,10 +1,33 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+import matplotlib
 import warnings
 
 
-def directed_network_graph(self, **kwargs):
+def directed_network_graph(self, **kwargs) -> matplotlib.figure.Figure:
+
+    """Directed network graph with absolute and proportional win tuples in edges.
+
+    Parameters
+    ----------
+    :param kwargs: dict
+        Pass parameter for fig_size in matplotlib and pass parameters for draw_networkx method.
+
+    Returns
+    -------
+    figure : matplotlib.figure.Figure
+        Planar layout network graph figure. The nodes are named as indices passed to Hierarchia object.
+        The edges have absolute and proportional dyadic relationship indicators.
+
+    See also
+    --------
+    https://networkx.org/documentation/stable/reference/classes/digraph.html
+    https://networkx.org/documentation/stable/reference/generated/networkx.drawing.layout.planar_layout.html
+    https://networkx.org/documentation/stable/reference/generated/networkx.drawing.nx_pylab.draw_networkx.html
+    """
+
+    # Process matrix
     processed_mat = self.mat - np.transpose(self.mat)
     processed_mat = np.where(processed_mat < 0, 0, processed_mat)
 
@@ -15,12 +38,14 @@ def directed_network_graph(self, **kwargs):
     network_graph_int = nx.from_numpy_matrix(processed_mat, create_using=nx.DiGraph(directed=True))
     network_graph_int = nx.relabel_nodes(network_graph_int, {idx: name for idx, name in enumerate(self.indices)})
 
+    # Adjust figure size
     if 'fig_size' in kwargs:
         figure = plt.figure(figsize=kwargs['fig_size'])
         del kwargs['fig_size']
     else:
         figure = plt.figure()
 
+    # Try plot the network with planar layout
     try:
         pos = nx.planar_layout(network_graph)
         nx.draw_networkx(network_graph,
@@ -37,7 +62,6 @@ def directed_network_graph(self, **kwargs):
         plt.tight_layout()
         figure.canvas.draw()
         return figure
-
     except:
         warnings.warn('Layout is not suitable for planar!')
         return None
