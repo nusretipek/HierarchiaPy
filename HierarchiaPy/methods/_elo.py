b155ef81e685ee5cd2a2a04c06e9fecd8e568f0a
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-07-05 14:49:51 +0200

    Update _elo.py

--- HierarchiaPy/methods/_elo.py
+++ HierarchiaPy/methods/_elo.py
@@ -1,20 +1,19 @@
 from scipy.special import erfc
 
 
-def elo(self, start_value=1000, K=100, normal_probability=False):
+def elo(self, start_value: float = 1000, K: float = 100, normal_probability: bool = False) -> dict:
 
     """Elo rating from an interaction dataframe.
 
     Parameters
     ----------
-    :param self: reference to the current instance of the class
     :param start_value: float
         Parameter of the Elo function that determines the initial scores. It does not have
         an effect on relative differences after the calculation. (1000)
     :param K: float
         Parameter of the Elo function that acts as a factor and determines the speed at which scores change
         after an interaction. Optimization might be a good idea but not implemented yet. (100)
-    :param normal_probability: Boolean
+    :param normal_probability: bool
         Adjust the calculation of expected win/loss probabilities; default is Logistic,
         the normal probabilities are calculated using standard normal tables. For normalised probabilities,
         refer to https://handbook.fide.com (False)
@@ -33,7 +32,7 @@ def elo(self, start_value=1000, K=100, normal_probability=False):
     Notes
     -----
     From Wikipedia:
-    
+
     The Elo rating system is a method for calculating the relative skill levels of players
     in zero-sum games such as chess. It is named after its creator Arpad Elo, a Hungarian-American physics professor.
     The Elo system was originally invented as an improved chess-rating system over the previously used Harkness system,
@@ -52,18 +51,17 @@ def elo(self, start_value=1000, K=100, normal_probability=False):
     * Albers, P. C. H. & de Vries, H. 2001.
       Elo-rating as a tool in the sequential estimation of dominance strengths.
       Animal Behaviour, 61, 489-495. (DOI: 10.1006/anbe.2000.1571)
-
     """
 
-    # check whether Pandas dataframe provided
+    # Check whether Pandas dataframe provided
     if not hasattr(self, 'df'):
         raise ValueError('Elo rating depends on sequence of wins/loses, computation uses Pandas dataframe, '
                          'consider using randomized elo')
 
-    # initialize elo dictionary
+    # Initialize elo dictionary
     elo_dict = {i: start_value for i in self.indices}
 
-    # iterate through the dataframe rows and compute elo scores
+    # Iterate through the dataframe rows and compute elo scores
     for idx, row in self.df.iterrows():
 
         elo_diff = elo_dict[row[self.winner_col]] - elo_dict[row[self.loser_col]]
@@ -77,5 +75,6 @@ def elo(self, start_value=1000, K=100, normal_probability=False):
         elo_dict[row[self.winner_col]] += (K - K * expected_winner)
         elo_dict[row[self.loser_col]] += (-K * expected_loser)
 
-    # return final elo scores dictionary
-    return {key: round(elo_dict[key], 4) for key in elo_dict}
+    # Return final elo scores dictionary
+    elo_dict = {key: round(elo_dict[key], 4) for key in elo_dict}
+    return elo_dict
