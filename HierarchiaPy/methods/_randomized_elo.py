66396b0840cd46e7eeef7aa773403f50d2c28675
Nusret Ipek <76397495+nusretipek@users.noreply.github.com>
2022-07-05 15:15:47 +0200

    Update _randomized_elo.py

--- HierarchiaPy/methods/_randomized_elo.py
+++ HierarchiaPy/methods/_randomized_elo.py
@@ -3,51 +3,47 @@ import pandas as pd
 from scipy.special import erfc
 
 
-def randomized_elo(self, start_value=1000, K=100, n=1000, normal_probability=False):
+def randomized_elo(self, start_value: float = 1000, K: float = 100, n: int = 1000, normal_probability: bool = False) -> dict:
 
     """Randomized Elo rating from an interaction dataframe/matrix.
 
     Parameters
-    ----------    
+    ----------
     :param start_value: float
         Parameter of the Elo function that determines the initial scores. It does not have
         an effect on relative differences after the calculation. (1000)
-        
     :param K: float
         Parameter of the Elo function that acts as a factor and determines the speed at which scores change
         after an interaction. Optimization might be a good idea but not implemented yet. (100)
-        
     :param n: integer
         Parameter to adjust number of iterations for random ordering. Higher numbers result in more stable Elo ratings.
         The number of random orders. (1000)
-        
-    :param normal_probability: Boolean
+    :param normal_probability: bool
         Adjust the calculation of expected win/loss probabilities; default is Logistic,
         the normal probabilities are calculated using standard normal tables. For normalised probabilities,
         refer to https://handbook.fide.com (False)
-
-
+    
     Returns
     -------
-    elo_dict : dict
-        Elo ratings, keys are individual names derived from either the Dataframe or name sequence (user provided,
-        see class module for more details) and values are Elo scores (rounded to 4 decimal places)
-
+    randomized_elo_dict : dict
+        Randomized Elo ratings, keys are individual names derived from either the Dataframe or name sequence (user 
+        provided, see class module for more details) and values are Randomized Elo scores (rounded to 4 decimal places)
+    
     See also
     --------
     https://mathworld.wolfram.com/Erfc.html
     https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.erfc.html
-
+    
     Notes
     -----
     A practical guide for inferring reliable dominance hierarchies and estimating their uncertainty
     However, most behavioural studies assume that individual dominance rank is relatively stable over time.
-    We propose an improvement of the original Elo-rating based on randomizing the order in which interactions 
-    occurred (n = 1 000 randomizations throughout). Randomising the order that interactions are recorded produces 
-    slightly different individual Elo-ratings  each time, from which we can calculate a mean individual rank. 
+    We propose an improvement of the original Elo-rating based on randomizing the order in which interactions
+    occurred (n = 1 000 randomizations throughout). Randomising the order that interactions are recorded produces
+    slightly different individual Elo-ratings  each time, from which we can calculate a mean individual rank.
     This method also allows estimating the 95% range of individual ranks when run on a single interaction dataset.
     (Sánchez-Tójar et al, 2017)
-    
+
     References
     ----------
     * https://en.wikipedia.org/wiki/Elo_rating_system
@@ -55,19 +51,17 @@ def randomized_elo(self, start_value=1000, K=100, n=1000, normal_probability=Fal
     * Sánchez-Tójar, Alfredo & Schroeder, Julia & Farine, Damien. (2017).
       A practical guide for inferring reliable dominance hierarchies and estimating their uncertainty.
       bioRxiv. 111146. 10.1101/111146.
-
     """
 
-    # transform matrix to df
+    # Transform matrix to df
     interactions = np.array([[self.indices[ix], self.indices[iy]] for ix, iy in np.ndindex(self.mat.shape)
                              for _ in range(int(self.mat[ix, iy]))])
-    df = pd.DataFrame({'winner_col': interactions[:, 0],
-                       'loser_col': interactions[:, 1]})
+    df = pd.DataFrame({'winner_col': interactions[:, 0], 'loser_col': interactions[:, 1]})
 
-    # initialize elo dictionary
+    # Initialize elo dictionary
     elo_dict_master = {i: [] for i in self.indices}
 
-    # iterate n times
+    # Iterate n times
     for _ in range(n):
         random_df = df.sample(frac=1)
         elo_dict_temp = {i: start_value for i in self.indices}
@@ -87,9 +81,11 @@ def randomized_elo(self, start_value=1000, K=100, n=1000, normal_probability=Fal
 
         for key in elo_dict_temp:
             elo_dict_master[key].append(elo_dict_temp[key])
-    
-    # take the average of the elo ratings (n times)
+
+    # Take the average of the elo ratings (n times)
     for key in elo_dict_master:
         elo_dict_master[key] = sum(elo_dict_master[key]) / len(elo_dict_master[key])
-
-    return {key: round(elo_dict_master[key], 4) for key in elo_dict_master}
+    
+    # Return randomized Elo dictionary
+    randomized_elo_dict = {key: round(elo_dict_master[key], 4) for key in elo_dict_master}
+    return randomized_elo_dict
